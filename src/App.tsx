4e47ae72e7def28bb5466cac0a5509e7ddd07721
VedranSelak <selakvedran@gmail.com>
2021-07-27 00:56:41 +0200

    featched data from an api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import { useState } from "react";
-import { useQuery } from "react-query";
+import { useState, useEffect } from "react";
+import { Wrapper } from "./App.styles";
+
+export type ItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
 
 function App(): JSX.Element {
+  const [items, setItems] = useState<ItemType[]>([]);
+
+  const getItems = async () => {
+    const endpoint = `https://fakestoreapi.com/products`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    setItems(data);
+  };
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
   return <div className="App">Store</div>;
 }
 
