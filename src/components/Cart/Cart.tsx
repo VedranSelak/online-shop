05927d850338d0e0157050b3500b0bcc07162ef3
VedranSelak <selakvedran@gmail.com>
2021-07-27 15:56:25 +0200

    added displaying total

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -9,6 +9,10 @@ type Props = {
 };
 
 function Cart({ cartItems, addToCart, removeFromCart }: Props): JSX.Element {
+  const calcTotal = (items: ItemType[]) => {
+    return items.reduce((num: number, item) => num + item.amount * item.price, 0);
+  };
+
   return (
     <Wrapper>
       <h2>Your Shopping Cart</h2>
@@ -16,6 +20,7 @@ function Cart({ cartItems, addToCart, removeFromCart }: Props): JSX.Element {
       {cartItems?.map((item) => (
         <CartItem key={item.id} item={item} addToCart={addToCart} removeFromCart={removeFromCart} />
       ))}
+      <h2>Total: ${calcTotal(cartItems).toFixed(2)}</h2>
     </Wrapper>
   );
 }
